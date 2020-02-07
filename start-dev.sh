cc145dd19bee6904fcba6c2e8145d9a9eade9148
Simon Vareille <si.vareille@gmail.com>
2020-02-07 22:06:18 +0100

    Fix command.

--- start-dev.sh
+++ start-dev.sh
@@ -3,9 +3,8 @@
 systemctl start mongod
 systemctl status mongod
 
-mongo
-use keyserver-test
-db.createUser({ user:"keyserver-user", pwd:"trfepCpjhVrqgpXFWsEF", roles:[{ role:"readWrite", db:"keyserver-test" }] })
+(echo 'use keyserver-test';echo 'db.createUser({ user:"keyserver-user", pwd:"trfepCpjhVrqgpXFWsEF", roles:[{ role:"readWrite",db:"keyserver-test" }] })') | mongo
+
 
 export NODE_ENV=development
 
